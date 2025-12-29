552cf58947f6d48711c9f860b0919d1fadd86303
Biniam777 <benjaminmarkos666@gmail.com>
2025-12-29 07:22:43 +0300

    fix: resolve minor bug

---
+++ mini-reddit-clone-frontend/src/app/app.routes.server.ts
@@ -0,0 +1,15 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+// Modified at 1767982548
+];
+// Logic update: ggGxCkrFjEx0
+// Logic update: CixFSu7w0NHu
+// Logic update: F3xexFdBEXcI
+// Logic update: ivt4XKj6Vq7s
+// Logic update: MRZ2HbDKvJhF
+// Logic update: lNpW89aw8hgv
+// Logic update: YONRWH8v1ZgE
