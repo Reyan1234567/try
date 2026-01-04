11181553c352c882356c95bb5045bc5135ed3d41
Zennoon <yunus24kedir@gmail.com>
2026-01-04 08:20:20 +0300

    docs: update comments

---
+++ MiniRedditCloneApi/DTOs/Domain/DomainDTO.cs
@@ -0,0 +1,14 @@
+namespace MiniRedditCloneApi.DTOs.Domain
+{
+    public class DomainDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:15:59 PM
+# Update Fri, Jan  9, 2026  9:25:38 PM
+# Update Fri, Jan  9, 2026  9:34:23 PM
+// Logic update: 0k4moTwbVP8V
+// Logic update: 38uGIq6wivg6
+// Logic update: rWBFiZyqmak9
---
+++ MiniRedditCloneApi/DTOs/Domain/DomainIdListDTO.cs
@@ -0,0 +1,13 @@
+namespace MiniRedditCloneApi.DTOs.Domain
+{
+    public class DomainIdListDTO
+    {
+        public List<int> DomainIds { get; set; } = null!;
+    }
+}
+# File update Fri, Jan  9, 2026  9:15:59 PM
+# Update Fri, Jan  9, 2026  9:25:38 PM
+# Update Fri, Jan  9, 2026  9:34:23 PM
+// Logic update: ZyM5DmX2869s
+// Logic update: dUam4xv0h60q
+// Logic update: mw2KdxC4qRS3
