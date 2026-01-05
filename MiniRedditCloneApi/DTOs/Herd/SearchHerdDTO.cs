1cfbbfcdc40c3e48d4a2e5e159a36ac88ba7c2ea
eyosi168 <eyosisol123@gmail.com>
2026-01-05 08:40:16 +0300

    refactor: optimize data flow

---
+++ MiniRedditCloneApi/DTOs/Herd/SearchHerdDTO.cs
@@ -0,0 +1,13 @@
+namespace MiniRedditCloneApi.DTOs.Herd
+{
+    public class SearchHerdDTO
+    {
+        public string? Q { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:16:00 PM
+# Update Fri, Jan  9, 2026  9:25:40 PM
+# Update Fri, Jan  9, 2026  9:34:25 PM
+// Logic update: IPjr22CXFxcn
+// Logic update: ArN0wRdsrw9v
+// Logic update: 78FPDkyKOszh
