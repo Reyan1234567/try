a41105ca712dc10fffa7f740b37e2a6ae7c67748
Zennoon <yunus24kedir@gmail.com>
2026-01-05 07:25:38 +0300

    refactor: optimize data flow

---
+++ MiniRedditCloneApi/DTOs/Herd/HerdRulesDTO.cs
@@ -0,0 +1,13 @@
+namespace MiniRedditCloneApi.DTOs.Herd
+{
+    public class HerdRulesDTO
+    {
+        public string? Rules { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:16:00 PM
+# Update Fri, Jan  9, 2026  9:25:39 PM
+# Update Fri, Jan  9, 2026  9:34:24 PM
+// Logic update: QfRIeZJMfFx4
+// Logic update: pP9tLbwc6T2i
+// Logic update: n3LTqpzhlTCy
