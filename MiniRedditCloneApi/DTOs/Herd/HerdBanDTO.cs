731b8f538b37df0feed6a4fd478dfedb39806d50
eyosi168 <eyosisol123@gmail.com>
2026-01-05 07:10:27 +0300

    feat: implement module logic

---
+++ MiniRedditCloneApi/DTOs/Herd/HerdBanDTO.cs
@@ -0,0 +1,16 @@
+namespace MiniRedditCloneApi.DTOs.Herd
+{
+    public class HerdBanDTO
+    {
+        public required string NerdId { get; set; }
+        public required string Reason { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:15:59 PM
+# Update Fri, Jan  9, 2026  9:25:39 PM
+# Update Fri, Jan  9, 2026  9:34:24 PM
+// Logic update: MGeHbxGTOLfC
+// Logic update: Ss4yDFEd1Gxy
+// Logic update: T9o5tM5vg96D
+// Logic update: sS45aNPHs8QA
+// Logic update: 5RajrtR4oJTE
---
+++ MiniRedditCloneApi/DTOs/Herd/HerdDTO.cs
@@ -0,0 +1,18 @@
+namespace MiniRedditCloneApi.DTOs.Herd
+{
+    public class HerdDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:15:59 PM
+# Update Fri, Jan  9, 2026  9:25:39 PM
+# Update Fri, Jan  9, 2026  9:34:24 PM
+// Logic update: zSkiX1GpQ0dx
+// Logic update: jkuqRUWVMQvn
+// Logic update: tlF1T5skpL2F
+// Logic update: wC1gQk279HEH
+// Logic update: pDSEHgBsMf50
