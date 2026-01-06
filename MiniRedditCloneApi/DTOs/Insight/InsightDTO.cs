e09fd0b75fd182ef2cd43742e783ff88ede26268
Reyan1234567 <reyann.14b@gmail.com>
2026-01-06 07:52:49 +0300

    docs: update comments

---
+++ MiniRedditCloneApi/DTOs/Insight/InsightDTO.cs
@@ -0,0 +1,27 @@
+using MiniRedditCloneApi.Models;
+
+namespace MiniRedditCloneApi.DTOs.Insight
+{
+    public class InsightDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public required string Title { get; set; }
+        public string? Text { get; set; }
+        public bool IsNSFW { get; set; }
+        public InsightStatus Status { get; set; }
+        public int Kudos { get; set; }
+        public int Crits { get; set; }
+        public int Notes { get; set; }
+        public string? MediaUrl { get; set; }
+    }
+}# File update Fri, Jan  9, 2026  9:16:01 PM
+# Update Fri, Jan  9, 2026  9:25:41 PM
+# Update Fri, Jan  9, 2026  9:34:26 PM
+// Logic update: VDfRzMWEiB1e
+// Logic update: hdEX2ljUVnp5
+// Logic update: YwyMZaCez7qP
+// Logic update: OAZakOqOtUBN
+// Logic update: yed3KV8O4J2F
+// Logic update: SfRilSWTICG4
+// Logic update: KsWvO2VL4JSs
---
+++ MiniRedditCloneApi/DTOs/Nerd/NerdConfirmEmailDTO.cs
@@ -0,0 +1,14 @@
+namespace MiniRedditCloneApi.DTOs.Nerd
+{
+    public class NerdConfirmationDTO(Models.Nerd nerd, string token)
+    {
+        public Models.Nerd Nerd { get; set; } = nerd;
+        public string Token { get; set; } = token;
+    }
+}
+# File update Fri, Jan  9, 2026  9:16:01 PM
+# Update Fri, Jan  9, 2026  9:25:41 PM
+# Update Fri, Jan  9, 2026  9:34:26 PM
+// Logic update: QymXryrHPGSK
+// Logic update: mhAQFEfopBBo
+// Logic update: yd9E3Mxf8y8W
