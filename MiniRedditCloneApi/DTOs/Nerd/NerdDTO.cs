a0e9d333c4e0039b4d49d1a5616bcd476f7b2c5b
Zennoon <yunus24kedir@gmail.com>
2026-01-06 08:20:37 +0300

    docs: update comments

---
+++ MiniRedditCloneApi/DTOs/Nerd/NerdDTO.cs
@@ -0,0 +1,18 @@
+namespace MiniRedditCloneApi.DTOs.Nerd
+{
+    public class NerdDTO
+    {
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public string? AvatarUrl { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:16:01 PM
+# Update Fri, Jan  9, 2026  9:25:41 PM
+# Update Fri, Jan  9, 2026  9:34:27 PM
+// Logic update: eqSpSoML98wx
+// Logic update: 1IxDFsCWKabM
+// Logic update: FzIiOHgodyVR
+// Logic update: xHEbLV6szCTu
+// Logic update: mLTAjWYUG1II
+// Logic update: Kgk72jwdSV5M
