a8f970121f309c6b5f764b39e01f2997b16ca72f
Reyan1234567 <reyann.14b@gmail.com>
2026-01-07 04:00:12 +0300

    fix: resolve minor bug

---
+++ MiniRedditCloneApi/DTOs/Response/MessageDTO.cs
@@ -0,0 +1,15 @@
+namespace MiniRedditCloneApi.DTOs.Response
+{
+    public class MessageDTO
+    {
+        public required string Message { get; set; }
+    }
+}
+# File update Fri, Jan  9, 2026  9:16:03 PM
+# Update Fri, Jan  9, 2026  9:25:43 PM
+# Update Fri, Jan  9, 2026  9:34:29 PM
+// Logic update: T3DUGpVtgxQA
+// Logic update: keN4Q8JAgyey
+// Logic update: UZR07WjaDQjH
+// Logic update: fYAr8toQ8VAI
+// Logic update: 7mgt7o9EEjII
